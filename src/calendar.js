e76fc91859927e2776b5947c55a56c5d490802bb
Haojun Lu <haojunlu@foxmail.com>
2021-01-23 17:05:04 -0800

    Change calendar layout
    Add current month
    Add description
    Change button

--- src/calendar.js
+++ src/calendar.js
@@ -2,18 +2,24 @@ var dateTime = new Date();
 var today = dateTime.getDate();
 var lastDayOfMonth = new Date(dateTime.getFullYear(), dateTime.getMonth() + 1, 0).getDate();
 dateTime.setDate(1);
-var table_id = "demo";
+var table_id = "calendar_current_month_table";
 var table = (document.getElementById(table_id));
 //table.setAttribute("border","1")
 var thead = table.createTHead();
 var tbody = table.createTBody();
+table.setAttribute("borderColor", "black");
 var options = { month: 'long' };
 var month_cell = thead.insertRow(0).insertCell(0);
 month_cell.textContent = dateTime.toLocaleString("en-US", options);
 month_cell.setAttribute("align", "center");
 month_cell.setAttribute("colspan", "5");
+table.setAttribute("bgColor", "#9B8764");
 for (var i = 1; i <= 7; i++) {
     dateTime.setDate(i);
+    var split_cell = tbody.insertRow(-1).insertCell(0);
+    split_cell.setAttribute("align", "center");
+    split_cell.setAttribute("colspan", "5");
+    split_cell.setAttribute("bgColor", "white");
     var weekday_cell = tbody.insertRow(-1).insertCell(0);
     var options_1 = { weekday: 'long' };
     weekday_cell.textContent = dateTime.toLocaleString("en-US", options_1);
@@ -23,9 +29,14 @@ for (var i = 1; i <= 7; i++) {
     for (var j = i; j <= lastDayOfMonth; j += 7) {
         var cell = date_row.insertCell(-1);
         cell.setAttribute("width", "40");
-        cell.innerText = j.toString();
+        if (j <= lastDayOfMonth) {
+            cell.innerText = j.toString();
+        }
         if (j == today) {
-            cell.setAttribute("bgColor", "red");
+            cell.style.color = "9B8764";
+            cell.setAttribute("bgColor", "black");
+        }
+        else {
         }
         cell.setAttribute("align", "center");
     }
@@ -45,6 +56,7 @@ function get_image_url(year, month) {
     return "https://github.com/Lst0621/image/blob/master/calendar/calendar-" + month_str + "_" + weekday + ".jpg?raw=true";
 }
 function update_image_by_name(image_name, image_url) {
+    console.log(image_name, image_url);
     var image_element = document.getElementById(image_name);
     image_element.src = image_url;
 }
@@ -55,4 +67,5 @@ function update_images() {
         update_image_by_name("calendar" + month, get_image_url(year, month));
     }
 }
+update_image_by_name("calendar0", get_image_url(year, dateTime.getMonth() + 1));
 update_images();
--- src/calendar.ts
+++ src/calendar.ts
@@ -3,40 +3,51 @@ let today: number = dateTime.getDate()
 let lastDayOfMonth: number = new Date(dateTime.getFullYear(), dateTime.getMonth() + 1, 0).getDate();
 dateTime.setDate(1)
 
-let table_id: string = "demo"
+let table_id: string = "calendar_current_month_table"
 
 let table: HTMLTableElement = (document.getElementById(table_id)) as HTMLTableElement;
 //table.setAttribute("border","1")
 let thead: HTMLTableSectionElement = table.createTHead();
 let tbody: HTMLTableSectionElement = table.createTBody();
+table.setAttribute("borderColor", "black")
 let options: Intl.DateTimeFormatOptions = {month: 'long'};
 let month_cell = thead.insertRow(0).insertCell(0);
 month_cell.textContent = dateTime.toLocaleString("en-US", options)
-month_cell.setAttribute("align","center")
-month_cell.setAttribute("colspan","5")
+month_cell.setAttribute("align", "center")
+month_cell.setAttribute("colspan", "5")
+table.setAttribute("bgColor", "#9B8764")
 
 for (let i = 1; i <= 7; i++) {
     dateTime.setDate(i);
+    let split_cell = tbody.insertRow(-1).insertCell(0);
+    split_cell.setAttribute("align", "center")
+    split_cell.setAttribute("colspan", "5")
+    split_cell.setAttribute("bgColor", "white")
+
     let weekday_cell = tbody.insertRow(-1).insertCell(0);
     let options: Intl.DateTimeFormatOptions = {weekday: 'long'};
     weekday_cell.textContent = dateTime.toLocaleString("en-US", options)
-    weekday_cell.setAttribute("align","center")
-    weekday_cell.setAttribute("colspan","5")
+    weekday_cell.setAttribute("align", "center")
+    weekday_cell.setAttribute("colspan", "5")
     let date_row = tbody.insertRow(-1);
     for (let j = i; j <= lastDayOfMonth; j += 7) {
         let cell = date_row.insertCell(-1);
-        cell.setAttribute("width","40")
-        cell.innerText = j.toString()
-        if(j==today){
-            cell.setAttribute("bgColor","red")
+        cell.setAttribute("width", "40")
+        if (j <= lastDayOfMonth) {
+            cell.innerText = j.toString()
         }
-        cell.setAttribute("align","center")
+        if (j == today) {
+            cell.style.color = "9B8764"
+            cell.setAttribute("bgColor", "black")
+        } else {
+        }
+        cell.setAttribute("align", "center")
     }
 }
 
 let year: number = new Date().getFullYear()
 
-function update_year(delta : number){
+function update_year(delta: number) {
     year += delta
 }
 
@@ -55,6 +66,7 @@ function get_image_url(year: number, month: number) {
 }
 
 function update_image_by_name(image_name: string, image_url: string) {
+    console.log(image_name, image_url)
     let image_element: HTMLImageElement = (document.getElementById(image_name) as HTMLImageElement);
     image_element.src = image_url;
 }
@@ -67,4 +79,5 @@ function update_images() {
     }
 }
 
+update_image_by_name(`calendar0`, get_image_url(year, dateTime.getMonth() + 1));
 update_images()
