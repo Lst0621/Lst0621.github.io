56a8adfcd9786bf905abcccc189cacacb753622e
Haojun Lu <haojunlu@foxmail.com>
2022-01-04 15:03:33 -0800

    animation for tro

--- src/draw_trochoid.js
+++ src/draw_trochoid.js
@@ -4,6 +4,13 @@ var clear_prev = document.getElementById("clear_prev");
 var count_per_round = get_int("count_per_round");
 var width = canvas.width;
 var half_width = width / 2;
+var R = 100;
+var r_R_ratio_a = get_int("r_R_ratio_a");
+var r_R_ratio_b = get_int("r_R_ratio_b");
+var d_R_ratio = get_float("d_R_ratio");
+var rotate = get_float("rotate") / 360 * 2 * Math.PI;
+var timeout = 50;
+var call_back_id = 0;
 function gcd(a, b) {
     if (a == b) {
         return a;
@@ -17,13 +24,17 @@ function gcd(a, b) {
     }
     return gcd(b % a, a);
 }
+function get_moving_circle_center_x(R, r, theta, delta) {
+    return (R - r) * Math.cos(theta + delta);
+}
+function get_moving_circle_center_y(R, r, theta, delta) {
+    return (R - r) * Math.sin(theta + delta);
+}
 function get_x(R, r, d, theta, delta) {
-    var x = (R - r) * Math.cos(theta + delta) + d * Math.cos((R - r) / r * theta + delta);
-    return x;
+    return get_moving_circle_center_x(R, r, theta, delta) + d * Math.cos((R - r) / r * theta + delta);
 }
 function get_y(R, r, d, theta, delta) {
-    var y = (R - r) * Math.sin(theta + delta) - d * Math.sin((R - r) / r * theta + delta);
-    return y;
+    return get_moving_circle_center_y(R, r, theta, delta) - d * Math.sin((R - r) / r * theta + delta);
 }
 function get_dp(R, r, d, delta, round, count) {
     var dp_x = [];
@@ -38,15 +49,11 @@ function get_dp(R, r, d, delta, round, count) {
     coordinates.push(dp_y);
     return coordinates;
 }
+function clear_canvas() {
+    context.fillRect(0, 0, canvas.width, canvas.height);
+}
 function draw(R, r_R_ratio_a, r_R_ratio_b, d_R_ratio, delta) {
     console.log("draw ", R, r_R_ratio_a, r_R_ratio_b, d_R_ratio);
-    if (clear_prev.checked) {
-        context.fillRect(0, 0, canvas.width, canvas.height);
-        console.log("clear");
-    }
-    else {
-        console.log("keep previously drawn");
-    }
     var r_R_ratio = r_R_ratio_a / r_R_ratio_b;
     var round = r_R_ratio_b / gcd(r_R_ratio_a, r_R_ratio_b);
     console.log("round " + round);
@@ -72,15 +79,64 @@ function get_int(element_id) {
 function get_float(element_id) {
     return parseFloat(document.getElementById(element_id).value);
 }
-function update() {
-    var R = 100;
-    var r_R_ratio_a = get_int("r_R_ratio_a");
-    var r_R_ratio_b = get_int("r_R_ratio_b");
-    var d_R_ratio = get_float("d_R_ratio");
-    var rotate = get_float("rotate");
+function draw_animation(theta, delta) {
+    // draw outer
+    context.beginPath();
+    context.arc(half_width, half_width, R, 0, 2 * Math.PI);
+    context.stroke();
+    // draw moving center traj
+    var r_R_ratio = r_R_ratio_a / r_R_ratio_b;
+    var r = R * r_R_ratio;
+    context.beginPath();
+    context.arc(half_width, half_width, R - r, 0, 2 * Math.PI);
+    context.stroke();
+    // draw moving
+    var moving_center_x = half_width + get_moving_circle_center_x(R, r, theta, delta);
+    var moving_center_y = half_width + get_moving_circle_center_y(R, r, theta, delta);
+    context.beginPath();
+    context.arc(moving_center_x, moving_center_y, r, 0, 2 * Math.PI);
+    context.stroke();
+    //draw line
+    var d = R * d_R_ratio;
+    var trochoid_x = half_width + get_x(R, r, d, theta, delta);
+    var trochoid_y = half_width + get_y(R, r, d, theta, delta);
+    context.beginPath();
+    context.moveTo(moving_center_x, moving_center_y);
+    context.lineTo(trochoid_x, trochoid_y);
+    context.stroke();
+}
+function read_paras() {
+    r_R_ratio_a = get_int("r_R_ratio_a");
+    r_R_ratio_b = get_int("r_R_ratio_b");
+    d_R_ratio = get_float("d_R_ratio");
+    rotate = get_float("rotate");
     count_per_round = get_int("count_per_round");
-    draw(R, r_R_ratio_a, r_R_ratio_b, d_R_ratio, rotate / 360 * 2 * Math.PI);
+}
+function draw_once(theta) {
+    if (clear_prev.checked) {
+        clear_canvas();
+        draw_animation(theta, rotate);
+    }
+    draw(R, r_R_ratio_a, r_R_ratio_b, d_R_ratio, rotate);
+    if (clear_prev.checked) {
+        var factor_1 = 20;
+        call_back_id = setTimeout(function () {
+            draw_once(theta + 2 * Math.PI / factor_1);
+        }, timeout);
+    }
+}
+function update() {
+    clearTimeout(call_back_id);
+    read_paras();
+    if (clear_prev.checked) {
+        console.log("clear");
+        clear_canvas();
+    }
+    else {
+        console.log("keep previously drawn");
+    }
+    draw_once(0);
 }
 context.fillStyle = "#000000";
-context.fillRect(0, 0, canvas.width, canvas.height);
+clear_canvas();
 update();
--- src/draw_trochoid.ts
+++ src/draw_trochoid.ts
@@ -6,6 +6,14 @@ let count_per_round: number = get_int("count_per_round")
 
 let width: number = canvas.width
 let half_width: number = width / 2
+let R: number = 100
+let r_R_ratio_a: number = get_int("r_R_ratio_a")
+let r_R_ratio_b: number = get_int("r_R_ratio_b")
+let d_R_ratio: number = get_float("d_R_ratio")
+let rotate: number = get_float("rotate") / 360 * 2 * Math.PI
+
+let timeout: number = 50
+let call_back_id: number = 0
 
 function gcd(a: number, b: number) {
     if (a == b) {
@@ -24,14 +32,20 @@ function gcd(a: number, b: number) {
     return gcd(b % a, a)
 }
 
+function get_moving_circle_center_x(R: number, r: number, theta: number, delta: number) {
+    return (R - r) * Math.cos(theta + delta)
+}
+
+function get_moving_circle_center_y(R: number, r: number, theta: number, delta: number) {
+    return (R - r) * Math.sin(theta + delta)
+}
+
 function get_x(R: number, r: number, d: number, theta: number, delta: number) {
-    let x: number = (R - r) * Math.cos(theta + delta) + d * Math.cos((R - r) / r * theta + delta)
-    return x
+    return get_moving_circle_center_x(R, r, theta, delta) + d * Math.cos((R - r) / r * theta + delta)
 }
 
 function get_y(R: number, r: number, d: number, theta: number, delta: number) {
-    let y: number = (R - r) * Math.sin(theta + delta) - d * Math.sin((R - r) / r * theta + delta)
-    return y
+    return get_moving_circle_center_y(R, r, theta, delta) - d * Math.sin((R - r) / r * theta + delta)
 }
 
 function get_dp(R: number, r: number, d: number, delta: number, round: number, count: number) {
@@ -48,14 +62,12 @@ function get_dp(R: number, r: number, d: number, delta: number, round: number, c
     return coordinates
 }
 
+function clear_canvas() {
+    context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
 function draw(R: number, r_R_ratio_a: number, r_R_ratio_b: number, d_R_ratio: number, delta: number) {
     console.log("draw ", R, r_R_ratio_a, r_R_ratio_b, d_R_ratio)
-    if (clear_prev.checked) {
-        context.fillRect(0, 0, canvas.width, canvas.height);
-        console.log("clear")
-    } else {
-        console.log("keep previously drawn")
-    }
     let r_R_ratio: number = r_R_ratio_a / r_R_ratio_b
     let round: number = r_R_ratio_b / gcd(r_R_ratio_a, r_R_ratio_b)
     console.log("round " + round)
@@ -84,17 +96,73 @@ function get_float(element_id: string) {
     return parseFloat((document.getElementById(element_id) as HTMLInputElement).value)
 }
 
-function update() {
-    let R: number = 100
-    let r_R_ratio_a: number = get_int("r_R_ratio_a")
-    let r_R_ratio_b: number = get_int("r_R_ratio_b")
-    let d_R_ratio: number = get_float("d_R_ratio")
-    let rotate: number = get_float("rotate")
+function draw_animation(theta: number, delta: number) {
+    // draw outer
+    context.beginPath()
+    context.arc(half_width, half_width, R, 0, 2 * Math.PI)
+    context.stroke()
+
+    // draw moving center traj
+    let r_R_ratio: number = r_R_ratio_a / r_R_ratio_b
+    let r: number = R * r_R_ratio
+
+    context.beginPath()
+    context.arc(half_width, half_width, R - r, 0, 2 * Math.PI)
+    context.stroke()
+
+    // draw moving
+    let moving_center_x: number = half_width + get_moving_circle_center_x(R, r, theta, delta)
+    let moving_center_y: number = half_width + get_moving_circle_center_y(R, r, theta, delta)
+
+    context.beginPath()
+    context.arc(moving_center_x, moving_center_y, r, 0, 2 * Math.PI)
+    context.stroke()
+
+    //draw line
+    let d: number = R * d_R_ratio
+    let trochoid_x: number = half_width + get_x(R, r, d, theta, delta)
+    let trochoid_y: number = half_width + get_y(R, r, d, theta, delta)
+    context.beginPath()
+    context.moveTo(moving_center_x, moving_center_y)
+    context.lineTo(trochoid_x, trochoid_y)
+    context.stroke()
+}
+
+function read_paras() {
+    r_R_ratio_a = get_int("r_R_ratio_a")
+    r_R_ratio_b = get_int("r_R_ratio_b")
+    d_R_ratio = get_float("d_R_ratio")
+    rotate = get_float("rotate")
     count_per_round = get_int("count_per_round")
-    draw(R, r_R_ratio_a, r_R_ratio_b, d_R_ratio, rotate / 360 * 2 * Math.PI)
+}
+
+function draw_once(theta: number) {
+    if (clear_prev.checked) {
+        clear_canvas()
+        draw_animation(theta, rotate)
+    }
+    draw(R, r_R_ratio_a, r_R_ratio_b, d_R_ratio, rotate)
+    if (clear_prev.checked) {
+        let factor: number = 20
+        call_back_id = setTimeout(function () {
+            draw_once(theta + 2 * Math.PI / factor)
+        }, timeout)
+    }
+}
+
+function update() {
+    clearTimeout(call_back_id)
+    read_paras()
+    if (clear_prev.checked) {
+        console.log("clear")
+        clear_canvas()
+    } else {
+        console.log("keep previously drawn")
+    }
+    draw_once(0)
 }
 
 context.fillStyle = "#000000";
-context.fillRect(0, 0, canvas.width, canvas.height);
+clear_canvas()
 
 update()
