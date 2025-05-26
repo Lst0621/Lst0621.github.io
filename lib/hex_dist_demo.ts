cfe9524f43f36ff27db4892ba2953574803bca6c
Haojun Lu <haojunlu@foxmail.com>
2025-05-26 11:44:59 -0700

    Hex Dist: Add a Pause Button

--- lib/hex_dist_demo.ts
+++ lib/hex_dist_demo.ts
@@ -8,25 +8,27 @@ let ctx: CanvasRenderingContext2D = canvas.getContext("2d") as CanvasRenderingCo
 let to: number = 0
 let x: number = 15
 let y: number = 10
+let x_max = 34
+let y_max = 40
 
 function loop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    if (!paused) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-    let x_max = 34
-    let y_max = 40
-    x += Math.floor(Math.random() * 4 - 2)
-    y += Math.floor(Math.random() * 4 - 2)
-    if (x < 0) {
-        x += x_max
+        x += Math.floor(Math.random() * 4 - 2)
+        y += Math.floor(Math.random() * 4 - 2)
+        if (x < 0) {
+            x += x_max
+        }
+        if (y < 0) {
+            y += y_max
+        }
+        x = x % x_max
+        y = y % y_max
+        console.log(x, y)
+        drawHexGrid(ctx, x_max, y_max, 10, (col: number, row: number): string => get_color(x, y, col, row));
     }
-    if (y < 0) {
-        y += y_max
-    }
-    x = x % x_max
-    y = y % y_max
-    console.log(x, y)
 
-    drawHexGrid(ctx, x_max, y_max, 10, (col: number, row: number): string => get_color(x, y, col, row));
     let timeout: number = 300
     to = setTimeout(loop, timeout)
 }
@@ -45,4 +47,15 @@ function get_color(center_x: number, center_y: number, col: number, row: number)
     return color
 }
 
-restart()
+let paused: boolean = false
+
+export function pause() {
+    paused = !paused
+}
+
+function start() {
+    document.getElementById("pause")?.addEventListener('click', pause)
+    restart()
+}
+
+start()
