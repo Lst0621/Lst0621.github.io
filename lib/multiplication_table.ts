c540988e50ed168e79ad422bc75c9863e5491cc2
Haojun Lu <haojunlu@foxmail.com>
2025-06-05 23:43:08 -0700

    Multiply: Style

--- lib/multiplication_table.ts
+++ lib/multiplication_table.ts
@@ -4,6 +4,7 @@ export function update_table(sz: number) {
     let table: HTMLTableElement = document.getElementById("multiplication_table") as HTMLTableElement
     table.style.alignSelf = "center"
     table.style.borderStyle = "solid"
+    table.style.textAlign = "center"
     while (true) {
         if (table.rows.length == 0) {
             break
@@ -29,6 +30,7 @@ export function update_table(sz: number) {
         cell.innerText = i.toString()
         for (let j = 1; j <= sz; j++) {
             let cell_product = row.insertCell()
+            cell_product.style.borderStyle = "solid"
             cell_product.innerText = (i * j % (sz + 1)).toString()
         }
     }
