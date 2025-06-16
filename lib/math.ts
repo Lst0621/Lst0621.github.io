1bea8832eccf6b57fde0ce8d097647aaf144cbd1
Haojun Lu <haojunlu@foxmail.com>
2025-06-15 22:46:29 -0700

    Multiplication Table for Permutation

--- lib/math.ts
+++ lib/math.ts
@@ -29,3 +29,93 @@ export function gcd(a_in: number, b_in: number): number {
 export function are_co_prime(a: number, b: number): boolean {
     return gcd(a, b) == 1
 }
+
+export function permutation_multiply(p1: number[], p2: number[]): number[] {
+    let l1 = p1.length;
+    let l2 = p2.length;
+    let l = Math.max(l1, l2);
+    let ans: number[] = []
+    for (let i = 1; i <= l; i++) {
+        let p2_i = p2[i - 1]
+        let p1_p2_i: number = p1[p2_i - 1]
+        ans.push(p1_p2_i)
+    }
+    return ans
+}
+
+export function get_all_permutations(n: number): number[][] {
+    if (n == 1) {
+        return [[1]]
+    }
+    let per_n_minus_one = get_all_permutations(n - 1)
+    let ans: number[][] = []
+    for (let permutation of per_n_minus_one) {
+        for (let i: number = 0; i <= n - 1; i++) {
+            let cp: number[] = Array.from(permutation)
+            cp.splice(n - 1 - i, 0, n)
+            ans.push(cp)
+        }
+    }
+    return ans
+}
+
+export function get_cycles_from_permutations(perm: number[]) {
+    let visited = Array(perm.length).fill(0)
+    let cycles: number[][] = []
+    let cycle: number[] = []
+    let i = 0
+    while (true) {
+        if (i == perm.length) {
+            break
+        }
+        if (visited[i] == true) {
+            if (cycle.length != 0) {
+                cycles.push(Array.from(cycle))
+            }
+            cycle = []
+            i = i + 1
+            continue
+        }
+        let from: number = i + 1
+        let to: number = perm[i]
+        visited[i] = true
+        cycle.push(from)
+        i = to - 1
+    }
+    return cycles
+}
+
+export function get_permutation_parity(perm: number[]) {
+    let cycles: number[][] = get_cycles_from_permutations(perm)
+    let parity: boolean = true
+    for (let cycle of cycles) {
+        let is_cycle_odd = cycle.length % 2 == 0
+        parity = (parity && !is_cycle_odd) || (!parity && is_cycle_odd)
+    }
+    return parity
+}
+
+export function per_to_str(perm: number[]) {
+    let cycles: number[][] = get_cycles_from_permutations(perm)
+    console.log(perm)
+    console.log(cycles)
+
+    let ret = ""
+    for (let cycle of cycles) {
+        if (cycle.length <= 1) {
+            continue
+        }
+        let c = ""
+        for (let j = 0; j < cycle.length; j++) {
+            if (j >= 1) {
+                c += ","
+            }
+            c += cycle[j]
+        }
+        ret += "(" + cycle + ")"
+    }
+    if (ret.length == 0) {
+        ret = "e"
+    }
+    return ret
+}
---
+++ lib/permute_multiplication_table.ts
@@ -0,0 +1,76 @@
+import {get_all_permutations, get_permutation_parity, per_to_str, permutation_multiply} from "./math.js"
+
+let table_sz: number = 4
+
+
+export function update_table(sz: number) {
+    let mul_text = document.getElementById("mul_text") as HTMLSpanElement
+    mul_text.innerText = "Multiplication for S" + (sz).toString()
+    let table: HTMLTableElement = document.getElementById("multiplication_table") as HTMLTableElement
+    table.style.alignSelf = "center"
+    table.style.borderStyle = "solid"
+    table.style.textAlign = "center"
+    while (true) {
+        if (table.rows.length == 0) {
+            break
+        }
+        table.deleteRow(0)
+    }
+
+    let perms = get_all_permutations(sz)
+    console.log(perms)
+    let fac = perms.length
+
+    {
+        let row = table.insertRow()
+        for (let i = 0; i <= fac; i++) {
+            let cell = row.insertCell()
+            if (i != 0) {
+                cell.style.borderStyle = "solid"
+                cell.innerText = per_to_str(perms[i - 1])
+                if (get_permutation_parity(perms[i - 1])) {
+                    cell.style.background = "lightgreen"
+                } else {
+                    cell.style.background = "lightblue"
+                }
+            }
+        }
+    }
+
+    for (let i = 1; i <= fac; i++) {
+        let row = table.insertRow()
+        let cell = row.insertCell()
+        cell.style.borderStyle = "solid"
+        cell.innerText = per_to_str(perms[i - 1])
+        if (get_permutation_parity(perms[i - 1])) {
+            cell.style.background = "lightgreen"
+        } else {
+            cell.style.background = "lightblue"
+        }
+        for (let j = 1; j <= fac; j++) {
+            let cell_product = row.insertCell()
+            cell_product.style.borderStyle = "solid"
+            let product = permutation_multiply(perms[i - 1], perms[j - 1]);
+            cell_product.innerText = per_to_str(product)
+            if (get_permutation_parity(product)) {
+                cell_product.style.background = "lightgreen"
+            } else {
+                cell_product.style.background = "lightblue"
+            }
+        }
+    }
+}
+
+export function increment(sz: number) {
+    if (table_sz < 5) {
+        table_sz += 1
+    }
+    update_table(table_sz)
+}
+
+export function decrement(sz: number) {
+    if (table_sz > 2) {
+        table_sz -= 1
+    }
+    update_table(table_sz)
+}
