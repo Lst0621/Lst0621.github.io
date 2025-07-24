841fc384ff757da0196ad3f8ef78e0ebbb7a3ea9
Haojun Lu <haojunlu@foxmail.com>
2025-07-24 10:44:24 -0700

    Update Multiply
    Use draw_multiplication_table
    Add more tables

--- lib/dihedral_multiplication_table.ts
+++ lib/dihedral_multiplication_table.ts
@@ -1,7 +1,11 @@
 import {
-    dihedral_multiply, dihedral_to_str,
-    get_all_dihedral
+    dihedral_multiply, dihedral_to_permutation,
+    dihedral_to_str,
+    get_all_dihedral,
+    get_permutation_parity, perm_to_str,
+    permutation_multiply
 } from "./tsl/math.js"
+import {draw_multiplication_table} from "./tsl/visual.js";
 
 let table_sz: number = 4
 
@@ -10,46 +14,25 @@ export function update_table(sz: number) {
     let mul_text = document.getElementById("mul_text") as HTMLSpanElement
     mul_text.innerHTML = "Multiplication for D" + "<sub>" + (sz).toString() + "</sub>"
     let table: HTMLTableElement = document.getElementById("multiplication_table") as HTMLTableElement
-    table.style.alignSelf = "center"
-    table.style.borderStyle = "solid"
-    table.style.textAlign = "center"
-    while (true) {
-        if (table.rows.length == 0) {
-            break
-        }
-        table.deleteRow(0)
-    }
-
-    let dihedrals = get_all_dihedral(sz)
+    let table2: HTMLTableElement = document.getElementById("multiplication_table_perm") as HTMLTableElement
+    let dihedrals:number[][] = get_all_dihedral(sz)
     console.log(dihedrals)
-    let len: number = dihedrals.length
+    draw_multiplication_table(
+        table,
+        dihedrals,
+        (a, b) => dihedral_multiply(a, b, sz),
+        dihedral_to_str,
+        (a: number[]) => "lightblue",
+        (a: number[], b: number[], c: number[]) => "lightgreen")
 
-    {
-        let row = table.insertRow()
-        for (let i = 0; i <= len; i++) {
-            let cell = row.insertCell()
-            if (i != 0) {
-                cell.style.borderStyle = "solid"
-                cell.innerHTML = dihedral_to_str(dihedrals[i - 1])
-                cell.style.background = "lightblue"
-            }
-        }
-    }
-
-    for (let i = 1; i <= len; i++) {
-        let row = table.insertRow()
-        let cell = row.insertCell()
-        cell.style.borderStyle = "solid"
-        cell.innerHTML = dihedral_to_str(dihedrals[i - 1])
-        cell.style.background = "lightblue"
-        for (let j = 1; j <= len; j++) {
-            let cell_product = row.insertCell()
-            cell_product.style.borderStyle = "solid"
-            let product = dihedral_multiply(dihedrals[i - 1], dihedrals[j - 1], sz);
-            cell_product.innerHTML = dihedral_to_str(product)
-            cell_product.style.background = "lightgreen"
-        }
-    }
+    draw_multiplication_table(
+        table2,
+        dihedrals.map(dihedral=> dihedral_to_permutation(dihedral,sz)),
+        permutation_multiply,
+        perm_to_str,
+        (a) => (get_permutation_parity(a) ? "lightgreen" : "lightblue"),
+        (b, c, a) => (get_permutation_parity(a) ? "lightgreen" : "lightblue")
+    )
 }
 
 export function increment(sz: number) {
--- lib/permute_multiplication_table.ts
+++ lib/permute_multiplication_table.ts
@@ -1,65 +1,33 @@
 import {get_all_permutations, get_permutation_parity, perm_to_str, permutation_multiply, totient} from "./tsl/math.js"
 import {get_sub} from "./tsl/util.js";
+import {draw_multiplication_table} from "./tsl/visual.js";
 
 let table_sz: number = 4
 
 
 export function update_table(sz: number) {
     let mul_text = document.getElementById("mul_text") as HTMLSpanElement
-    mul_text.innerHTML = "Multiplication for S" + get_sub(sz.toString())
-    let table: HTMLTableElement = document.getElementById("multiplication_table") as HTMLTableElement
-    table.style.alignSelf = "center"
-    table.style.borderStyle = "solid"
-    table.style.textAlign = "center"
-    while (true) {
-        if (table.rows.length == 0) {
-            break
-        }
-        table.deleteRow(0)
-    }
-
+    mul_text.innerHTML = "Multiplication for A" + get_sub(sz.toString()) + " and S" + get_sub(sz.toString())
+    let table_sn: HTMLTableElement = document.getElementById("multiplication_table") as HTMLTableElement
+    let table_even: HTMLTableElement = document.getElementById("even_multiplication_table") as HTMLTableElement
     let perms = get_all_permutations(sz)
     console.log(perms)
-    let fac = perms.length
-
-    {
-        let row = table.insertRow()
-        for (let i = 0; i <= fac; i++) {
-            let cell = row.insertCell()
-            if (i != 0) {
-                cell.style.borderStyle = "solid"
-                cell.innerText = perm_to_str(perms[i - 1])
-                if (get_permutation_parity(perms[i - 1])) {
-                    cell.style.background = "lightgreen"
-                } else {
-                    cell.style.background = "lightblue"
-                }
-            }
-        }
-    }
-
-    for (let i = 1; i <= fac; i++) {
-        let row = table.insertRow()
-        let cell = row.insertCell()
-        cell.style.borderStyle = "solid"
-        cell.innerText = perm_to_str(perms[i - 1])
-        if (get_permutation_parity(perms[i - 1])) {
-            cell.style.background = "lightgreen"
-        } else {
-            cell.style.background = "lightblue"
-        }
-        for (let j = 1; j <= fac; j++) {
-            let cell_product = row.insertCell()
-            cell_product.style.borderStyle = "solid"
-            let product = permutation_multiply(perms[i - 1], perms[j - 1]);
-            cell_product.innerText = perm_to_str(product)
-            if (get_permutation_parity(product)) {
-                cell_product.style.background = "lightgreen"
-            } else {
-                cell_product.style.background = "lightblue"
-            }
-        }
-    }
+    draw_multiplication_table(
+        table_sn,
+        perms,
+        permutation_multiply,
+        perm_to_str,
+        (a) => (get_permutation_parity(a) ? "lightgreen" : "lightblue"),
+        (b, c, a) => (get_permutation_parity(a) ? "lightgreen" : "lightblue")
+    )
+    draw_multiplication_table(
+        table_even,
+        perms.filter(get_permutation_parity),
+        permutation_multiply,
+        perm_to_str,
+        (a) => (get_permutation_parity(a) ? "lightgreen" : "lightblue"),
+        (b, c, a) => (get_permutation_parity(a) ? "lightgreen" : "lightblue")
+    )
 }
 
 export function increment(sz: number) {
