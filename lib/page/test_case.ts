de219fc0605142020a6d82645e114fcfb9d84d5c
Haojun Lu <haojunlu@foxmail.com>
2025-07-31 21:15:30 -0700

    Update Test for Mod and GL

--- lib/page/test_case.ts
+++ lib/page/test_case.ts
@@ -1,4 +1,10 @@
-import {test_cartesian_product, test_inner_product, test_matrix_multiply} from "../test/test_math.js";
+import {
+    test_cartesian_product,
+    test_generate_general_linear_group_zn_m,
+    test_inner_product,
+    test_matrix_multiply,
+    test_mod
+} from "../test/test_math.js";
 import {always} from "../tsl/func.js";
 import {draw_table} from "../tsl/visual.js";
 
@@ -79,7 +85,7 @@ export function draw_test_cases_table(table: HTMLTableElement, tests: (() => boo
 export function update_table() {
     let table: HTMLTableElement = document.getElementById("test_case_table") as HTMLTableElement
 
-    let tests = [test_matrix_multiply, test_cartesian_product, test_inner_product]
+    let tests = [test_matrix_multiply, test_cartesian_product, test_inner_product, test_mod,test_generate_general_linear_group_zn_m]
     tests.push(test_failure, test_error)
     draw_test_cases_table(table, tests)
 }
