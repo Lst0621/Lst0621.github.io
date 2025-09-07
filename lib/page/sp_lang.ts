3b6980e48ad344dbf244ef7c25e58a34139e7006
Haojun Lu <haojunlu@foxmail.com>
2025-09-06 17:21:32 -0700

    Update SP

--- lib/page/sp_lang.ts
+++ lib/page/sp_lang.ts
@@ -1,22 +1,39 @@
-import {get_all_prefixes} from "../tsl/lang/string.js";
+import {
+    concat_same_string_lists_leq_n_times,
+    get_all_prefixes,
+    get_alphabet_from_strings, get_regex_for_disalloweb_sub_seq, get_sub_seq_regex
+} from "../tsl/lang/string.js";
 import {create_2d_array, range} from "../tsl/util.js";
 import {cartesian_product} from "../tsl/math/set.js";
 
-function draw(s1: string, s2: string) {
+function update(s1: string, s2: string) {
     const canvas: HTMLCanvasElement = document.getElementById("canvas_id_sp") as HTMLCanvasElement
     const ctx: CanvasRenderingContext2D = canvas.getContext("2d") as CanvasRenderingContext2D;
+    let span: HTMLSpanElement = document.getElementById('sp_span') as HTMLSpanElement;
+    let span2: HTMLSpanElement = document.getElementById('sp_span_2') as HTMLSpanElement;
+
+    let disallowed = [s1, s2]
+    let l = Math.max(s1.length, s2.length);
+    let alphabet = get_alphabet_from_strings([s1, s2]);
+    console.log(alphabet)
+    let all_sub_seq = concat_same_string_lists_leq_n_times(alphabet, l)
+    let allowed_seq = Array.from(all_sub_seq).filter(x => !disallowed.includes(x))
+
+    span.innerHTML = "Disallowed subsequences: " + disallowed + "<br>"
+    span.innerHTML += "Regex: " + get_regex_for_disalloweb_sub_seq(disallowed) + "<br>"
+    span2.innerHTML += "Allowed subsequences: " + allowed_seq + "<br>"
 
     let pre1: string[] = get_all_prefixes(s1)
     let pre2: string[] = get_all_prefixes(s2)
     let l1 = pre1.length;
     let l2 = pre2.length;
-    let idx1 = range(0, pre1.length);
-    let idx2 = range(0, pre2.length);
+    let idx1 = range(0, l1);
+    let idx2 = range(0, l2);
     let idx = cartesian_product([idx1, idx2])
     let scale = 120
     let radius = 50
     let arrows: any[][] = []
-    let visited: number[][] = create_2d_array(pre1.length, pre2.length, 0)
+    let visited: number[][] = create_2d_array(l1, l2, 0)
     travel(0, 0, pre1, pre2, arrows, visited)
     for (let i = 0; i < idx.length; i++) {
         let a = idx[i][0]
@@ -31,7 +48,6 @@ function draw(s1: string, s2: string) {
         let arrow = arrows[i]
         let from: number[] = arrow[0]
         let to: number[] = arrow[1]
-        console.log(from, to)
         draw_arrow(ctx, [(from[0] + 1) * scale, (from[1] + 1) * scale], [(to[0] + 1) * scale, (to[1] + 1) * scale], arrow[2])
     }
 }
@@ -108,4 +124,4 @@ function draw_state(ctx: CanvasRenderingContext2D, name: string, pos: number[], 
     ctx.fillText(name, pos[0], pos[1]);
 }
 
-draw("abcba", "bacba")
+update("abcda", "bacbb")
