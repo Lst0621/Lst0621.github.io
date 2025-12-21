74f2db26a2eed3bce8a27b4ed8f574bc1fc261c4
Haojun Lu <haojunlu@foxmail.com>
2025-12-21 00:19:15 -0800

    Update Tests for D_k

--- lib/test/test_lang.ts
+++ lib/test/test_lang.ts
@@ -1,17 +1,15 @@
 import {get_alphabet_from_strings, get_concat_and_suffix_func} from "../tsl/lang/string.js";
-import {generate_semigroup, get_all_idempotent_elements} from "../tsl/math/semigroup.js";
-
-export function get_definite_k(alphabet: string[], k: number): string[] {
-    let concat_with_suffix = get_concat_and_suffix_func(k)
-    let strs = generate_semigroup(alphabet, concat_with_suffix, (a, b) => a == b);
-    let idempotents = get_all_idempotent_elements(strs, concat_with_suffix, (a, b) => a == b);
-    return idempotents
-}
+import {generate_semigroup, get_all_idempotent_elements, get_highest_idempotent_power} from "../tsl/math/semigroup.js";
 
 export function test_definite_k() {
     let alphabet = get_alphabet_from_strings(["abcd"])
     console.log(alphabet)
     let k = 3
-    let result = get_definite_k(alphabet, k)
-    return result.length == Math.pow(alphabet.length, k)
+    let concat_with_suffix = get_concat_and_suffix_func(k)
+    let strs = generate_semigroup(alphabet, concat_with_suffix, (a, b) => a == b);
+    if (get_highest_idempotent_power(strs, concat_with_suffix, (a, b) => a == b) != k) {
+        return false
+    }
+    let idempotents = get_all_idempotent_elements(strs, concat_with_suffix, (a, b) => a == b);
+    return idempotents.length == Math.pow(alphabet.length, k)
 }
