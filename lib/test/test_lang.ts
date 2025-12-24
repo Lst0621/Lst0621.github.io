882523299f5635b08f4c345748e197895659b16f
Haojun Lu <haojunlu@foxmail.com>
2025-12-23 22:26:03 -0600

    Use Equals Function

--- lib/test/test_lang.ts
+++ lib/test/test_lang.ts
@@ -5,27 +5,28 @@ import {
     get_definite_k,
     get_highest_idempotent_power, is_abelian
 } from "../tsl/math/semigroup.js";
+import {equals} from "../tsl/func.js";
 
 export function test_definite_k() {
     let alphabet = get_alphabet_from_strings(["abcd"])
     console.log(alphabet)
     let k = 4
     let concat_with_suffix = get_concat_and_suffix_func(k)
-    let strs = generate_semigroup(alphabet, concat_with_suffix, (a, b) => a == b);
-    if (get_highest_idempotent_power(strs, concat_with_suffix, (a, b) => a == b) != k) {
+    let strs = generate_semigroup(alphabet, concat_with_suffix, equals);
+    if (get_highest_idempotent_power(strs, concat_with_suffix, equals) != k) {
         console.log("failed at highest idempotent power")
         return false
     }
-    let idempotent_elements = get_all_idempotent_elements(strs, concat_with_suffix, (a, b) => a == b);
+    let idempotent_elements = get_all_idempotent_elements(strs, concat_with_suffix, equals);
     if (idempotent_elements.length != Math.pow(alphabet.length, k)) {
         console.log("failed at idempotent_elements size")
         return false
     }
-    if (get_definite_k(strs, concat_with_suffix, (a, b) => a == b) != k) {
+    if (get_definite_k(strs, concat_with_suffix, equals) != k) {
         console.log("failed at definite k")
         return false
     }
-    if (is_abelian(strs, concat_with_suffix, (a, b) => a == b)) {
+    if (is_abelian(strs, concat_with_suffix, equals)) {
         console.log("failed at abelian")
         return false
     }
