c6540a66ace3de304a72636c63cd624bd8097e9a
Haojun Lu <haojunlu@foxmail.com>
2025-12-31 19:32:29 -0600

    Update Last Commit

--- get_last_commit_time.sh
+++ get_last_commit_time.sh
@@ -8,9 +8,8 @@ workspace=$(dirname "$my_dir")
 history_repo=$workspace/website_history
 
 history_file="history.csv.tmp"
+absolute_history_file=$my_dir/$history_file
 history_file_target="$history_repo"/history.csv
-
-git_files=$(git ls-tree -r master --name-only)
 
 set -x
 
@@ -20,6 +19,11 @@ git pull
 cd -
 
 rm -f "$history_file"
+
+# Get main repository files
+git_files=$(git ls-tree -r master --name-only)
+
+# Process main repository files
 set +x
 for fn in $git_files
 do
@@ -27,11 +31,21 @@ do
     git log -1 --format=%cs "$fn" >> $history_file
 done
 
+# Process submodule files
+git submodule foreach --quiet --recursive "
+    echo \"$history_file\"
+    for file in \$(git ls-tree -r HEAD --name-only); do
+        echo  \"\$sm_path/\$file,\"
+        git log -1 --format=%cs \"\$file\"
+        echo -n \"\$sm_path/\$file,\" >> \"$absolute_history_file\"
+        git log -1 --format=%cs \"\$file\" >> \"$absolute_history_file\"
+    done
+"
+
 set -x
 mv $history_file "$history_file_target"
 
 cd "$history_repo" || return
-git pull
 git status
 git --no-pager diff "$history_file_target"
 
